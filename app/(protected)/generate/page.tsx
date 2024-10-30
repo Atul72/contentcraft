b57ae544696889ef2e77b55208e650cf6b3feafb
Atul72 <rai703537@gmail.com>
2024-10-30 11:36:47 +0530

    style home page and header

--- app/(protected)/generate/page.tsx
+++ app/(protected)/generate/page.tsx
@@ -2,21 +2,13 @@
 
 import { useSession } from "next-auth/react";
 
-import { signOutAction } from "@/action/logout";
-
 export default function GeneratePage() {
   const { data: session } = useSession();
-
-  const onClick = () => {
-    signOutAction();
-  };
 
   return (
     <>
       <p>{session?.user?.email}</p>
-      <span onClick={onClick} className="cursor-pointer">
-        <button>Logout</button>
-      </span>
+      <p>{session?.user?.image}</p>
     </>
   );
 }
--- custom-components/header.tsx
+++ custom-components/header.tsx
@@ -29,7 +29,9 @@ export default function Header() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
         <Link href="/" className="flex items-center">
           <CircleIcon className="h-6 w-6 text-orange-500" />
-          <span className="ml-2 text-xl font-semibold text-gray-900">ACME</span>
+          <span className="ml-2 text-xl font-semibold text-gray-900">
+            ContentCraft
+          </span>
         </Link>
         <div className="flex items-center space-x-4">
           <Link
@@ -42,7 +44,7 @@ export default function Header() {
             <DropdownMenu open={isMenuOpen} onOpenChange={setIsMenuOpen}>
               <DropdownMenuTrigger asChild>
                 <Avatar className="cursor-pointer size-9">
-                  <AvatarImage alt={user.name || ""} />
+                  <AvatarImage src={user?.image || ""} alt={user.name || ""} />
                   <AvatarFallback>
                     {user.email
                       ?.split(" ")
